ccc4100f2cf3e6be11b62faeb835e0d9d3a946ab
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2023-03-08 09:51:26 +0100

    use yum-config-manager to add opm repo
    the ssl certificate issues has been resolved

--- opm/scripts/setup_buildenv.sh
+++ opm/scripts/setup_buildenv.sh
@@ -2,11 +2,7 @@
 
 yum -y install epel-release centos-release-scl
 yum -y install yum-utils wget git ca-certificates rpm-build rpmdevtools createrepo environment-modules
-#yum-config-manager --add-repo https://www.opm-project.org/package/opm.repo
-wget --no-check-certificate https://www.opm-project.org/package/opm.repo -O /etc/yum.repos.d/opm.repo
-wget --no-check-certificate https://www.opm-project.org/package/RPM-GPG-KEY-OPM
-rpm --import RPM-GPG-KEY-OPM
-yum-config-manager --save --setopt=opm.sslverify=false
+yum-config-manager --add-repo https://www.opm-project.org/package/opm.repo
 
 mkdir -p /tmp/opm/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 mkdir -p /tmp/opm/current
--- verify/scripts/setup_runtime_env.sh
+++ verify/scripts/setup_runtime_env.sh
@@ -2,11 +2,7 @@
 
 yum -y install epel-release centos-release-scl
 yum -y install git wget
-#yum-config-manager --add-repo https://www.opm-project.org/package/opm.repo
-wget --no-check-certificate https://www.opm-project.org/package/opm.repo -O /etc/yum.repos.d/opm.repo
-wget --no-check-certificate https://www.opm-project.org/package/RPM-GPG-KEY-OPM
-rpm --import RPM-GPG-KEY-OPM
-yum-config-manager --save --setopt=opm.sslverify=false
+yum-config-manager --add-repo https://www.opm-project.org/package/opm.repo
 yum makecache
 
 yum -y install openmpi openmpi3 mpich
