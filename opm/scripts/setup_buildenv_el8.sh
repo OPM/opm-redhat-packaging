4b1882a9cb345d2c7efa3ef2f5f20f28d5b3a838
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2026-06-10 14:10:45 +0200

    opm: bump toolset to gcc 15

--- opm/scripts/setup_buildenv_el8.sh
+++ opm/scripts/setup_buildenv_el8.sh
@@ -20,7 +20,7 @@ dnf config-manager --add-repo current.repo
 dnf config-manager --set-enabled powertools
 
 printf "%%_topdir /tmp/opm/rpmbuild
-%%_toolset gcc-toolset-13
+%%_toolset gcc-toolset-15
 %%_smp_mflags -j%i
 %%_build_openmpi %i
 %%_build_mpich %i
